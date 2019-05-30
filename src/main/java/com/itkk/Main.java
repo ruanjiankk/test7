432fed96b1d335cbed2c29d9bbb032febe88d15c
kk <821017624@qq.com>
2019-05-30 20:04:39 +0800

    第一次提交

---
+++ src/main/java/com/itkk/Main.java
@@ -0,0 +1,12 @@
+package com.itkk;
+
+/**
+ * @Author: K&K
+ * @Date: 2019/5/30 19:49
+ * @Version 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("kk");
+    }
+}
